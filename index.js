9d39c775b82b74e7cb5b4e273883aa59ae049bd5
jhseo5589 <109267025+jhseo5589@users.noreply.github.com>
2022-08-25 01:31:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats
+
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats
+
+}
+
+function appendCat(name) {
+    var newArray = [...cats,name];
+    return newArray
+}
+
+function prependCat(name) {
+    var newArray = [name, ...cats];
+    return newArray
+
+}
+
+function removeLastCat(name) {
+    var newArray = cats.slice(0,cats.length-1);
+    return newArray
+
+}
+
+function removeFirstCat(name) {
+    var newArray = cats.slice(1)
+    return newArray
+}
